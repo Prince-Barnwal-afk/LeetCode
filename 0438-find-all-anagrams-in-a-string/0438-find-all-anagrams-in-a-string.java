ffc04219db7689c85a3ca97abfbc6cfa3a88727f
Prince Barnwal <prince7baranwal@gmail.com>
2026-07-28 22:51:54 +0530

    Time: 10 ms (80.73%), Space: 46.6 MB (84.11%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -0,0 +1,37 @@
+
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        if (s.length() < p.length()) return result;
+
+        int[] pCount = new int[26];
+        int[] sCount = new int[26];
+
+        for (char c : p.toCharArray()) {
+            pCount[c - 'a']++;
+        }
+
+        int windowSize = p.length();
+
+        for (int i = 0; i < s.length(); i++) {
+            sCount[s.charAt(i) - 'a']++;
+
+            if (i >= windowSize) {
+                sCount[s.charAt(i - windowSize) - 'a']--;
+            }
+
+            if (i >= windowSize - 1 && matches(sCount, pCount)) {
+                result.add(i - windowSize + 1);
+            }
+        }
+
+        return result;
+    }
+
+    private boolean matches(int[] a, int[] b) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
+    }
+}
