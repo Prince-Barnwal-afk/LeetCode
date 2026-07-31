b15ea71eabb99a95647428271b2a82b5eabe7467
Prince Barnwal <prince7baranwal@gmail.com>
2026-07-31 23:32:45 +0530

    Time: 4 ms (82.77%), Space: 42 MB (68.51%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        int left = 0;
+        int right = (int)Math.sqrt(c);
+
+        while(left<=right){
+            long sum = (long)left*left + (long)right*right;
+            if(sum==c)return true;
+            if(sum<c)left++;
+            else right--;
+            }
+            return false;
+        }
+        
+    }
