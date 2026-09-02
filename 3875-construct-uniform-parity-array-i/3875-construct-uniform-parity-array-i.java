a044924a0bbae814fff7983ad2de3e062adae6a4
Prince Barnwal <prince7baranwal@gmail.com>
2026-09-02 10:18:13 -0700

    Time: 1 ms (31.89%), Space: 44.6 MB (99.41%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,6 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // As proven mathematically, a uniform parity array can always be constructed
+        return true;
+    }
+}
