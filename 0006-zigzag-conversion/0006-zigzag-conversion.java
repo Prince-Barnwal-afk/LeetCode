c1c3dd364f45c12060f827646a8fe90883fb51b3
Prince Barnwal <prince7baranwal@gmail.com>
2026-09-17 14:19:40 +0530

    Time: 5 ms (50.64%), Space: 46.2 MB (94.21%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || s.length() <= numRows) return s;
+
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
+
+        int currRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows[currRow].append(c);
+            
+            // Change direction when reaching top or bottom row
+            if (currRow == 0 || currRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+
+            // Move row index up or down based on direction
+            if (goingDown) {
+                currRow += 1;
+            } else {
+                currRow -= 1;
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+        
+        return result.toString();
+    }
+}
