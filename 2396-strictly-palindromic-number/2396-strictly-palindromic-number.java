45b91b2bb0626d68e86349dc0502ae4f97f594ea
Prince Barnwal <prince7baranwal@gmail.com>
2026-09-19 00:10:19 +0530

    Time: 0 ms (100%), Space: 41.9 MB (75.26%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -1,5 +1,31 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        return false;
+        int base = 0;
+        int rem = 0;
+        int placeValue = 1;
+
+        for(int i = 2; i <= n-2; i++){
+            int x = n;
+            while(x > 0){
+                rem = x % i;
+                base += rem * placeValue;
+                placeValue *= 10;
+                x /= i; 
+            }
+            if(isPalindrome(base) == false) return false;
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(int base){
+        int reverse = 0;
+        int x = base;
+        while(x>0){
+            int digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x / 10;
+        }
+        if(reverse == base) return true;
+        else return false;
     }
 }
