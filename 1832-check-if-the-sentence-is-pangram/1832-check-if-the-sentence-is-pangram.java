c8b7caf89b459f1fbffb0ba08d855106ac52e12c
Prince Barnwal <prince7baranwal@gmail.com>
2026-09-23 23:30:14 +0530

    Time: 4 ms (19.95%), Space: 42.9 MB (51.52%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        HashSet<Character> hs = new HashSet<>();
+
+        for(int i = 0; i < sentence.length(); i++){
+            char ch = sentence.charAt(i);
+            hs.add(ch);
+        }
+        return hs.size() == 26;
+    }
+}
